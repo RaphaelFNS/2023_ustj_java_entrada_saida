4f331f61464f93075c1a7f7cca76b7b0595a56c8
Raphael Nascimento <raphael.nascimentodesousa@gmail.com>
2023-09-04 19:38:21 -0300

    apo02(input-output): declaração de variaveis

---
+++ SomaDoisNumeros.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+//pacote padrão: java.lang: importado automaticamente
+public class SomaDoisNumero{
+    public static void main(String [] args) {
+        //Declaração de variáveis 
+        Scanner leitor = new Scanner(System.in);
+        double po1;
+        double po2;
+        double resultado;
+
+        //Entrada
+
+        //Processamento
+
+        //Saida
+    }
+}
